5b553570b7197bdaecb5328cf5491b638fbc1c86
mateusoliver <>
2021-05-20 20:23:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+var parar = 0;
+while(parar == 0){
+console.log("Bem vindo a Matshoes");
+console.log("")
+console.log("Faça login para realizar sua compra!");
+console.log("")
+var tenis = 50;
+
+var usuario = prompt("Favor informar um usuário");
+var senha = prompt("Informar a senha");
+
+if(usuario == "Mateus" && senha == "12345"){
+console.log(" usuário e senha correto!")
+console.log("")
+console.log("Escolha uma cor - Preto, Branco ou Vermelho")
+console.log("")
+var preto = prompt ("")
+console.log("")
+switch(preto){
+        case 'Preto' :
+
+var numeroTenis = prompt ("Informe o número do seu tenis")
+console.log("")
+var endereco = prompt ("Digite seu endereço para o envio da mercadoria")
+console.log("")
+console.log("valor final")
+console.log(tenis + 15)
+for(var pagamento = prompt("o pagamento será a vista?"); pagamento <= 3; pagamento++){console.log(pagamento)}
+console.log("")
+
+
+console.log("Por razões de segurança, verifique seu ID")
+console.log("sua ID 0482")
+var id = prompt ("confirme seu ID")
+do {
+  console.log("obrigado por comprar conosco!")
+} while(id === 0482)
+
+        break
+
+        case 'Branco' :
+
+var escolhaNumero = prompt ("Informe o número do seu tenis")
+console.log("")
+var endereco = prompt ("Digite seu endereço para o envio da mercadoria")
+console.log("")
+console.log("valor final")
+console.log(tenis + 15)
+for(var pagamento = prompt("o pagamento será a vista?"); pagamento <= 3; pagamento++){console.log(pagamento)}
+console.log("")
+
+
+console.log("Por razões de segurança, verifique seu ID")
+console.log("sua ID 0482")
+var id = prompt ("confirme seu ID")
+do {
+  console.log("obrigado por comprar conosco!")
+} while(id === 0482)
+        break
+
+        case 'Vermelho' :
+
+var numerotenis = prompt ("Informe o número do seu tenis")
+console.log("")
+var endereco = prompt ("Digite seu endereço para o envio da mercadoria ")
+console.log("")
+console.log("valor final")
+console.log(tenis + 15)
+for(var pagamento = prompt("o pagamento será a vista?"); pagamento <= 3; e++){console.log(pagamento)}
+console.log("")
+
+
+console.log("Por razões de segurança, verifique seu ID")
+console.log("sua ID 0482")
+var id = prompt ("confirme seu ID")
+do {
+  console.log("obrigado por comprar conosco!")
+} while(id === 0482)
+        break
+
+}
+break}
+
+
+else if(usuario != 'Mateus' && senha != '12345'){
+console.log("Tente novamente")
+console.log("")}
+
+}
